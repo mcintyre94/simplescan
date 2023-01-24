460bd2a8f118728426d607f9830aeb91a24373fb
Callum McIntyre <mcintyre1994@gmail.com>
2023-01-24 20:59:18 +0000

    Add beam analytics tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <script src="https://beamanalytics.io/beam.min.js" data-token="fb33585e-24a2-4769-ade4-f81b34abab12" async></script>
+      </Head>
       <body className='bg-slate-900'>
         <Main />
         <NextScript />
